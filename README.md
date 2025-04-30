e03e9f195aefe645463b0fa712503db436665353
joshaw807 <joshaw807@gmail.com>
2025-04-29 21:30:51 -0400

    finished the game, lets you play 5 rounds

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Rock-Paper-Scissors
 This project is a rock paper scissors game coded in javascript.
+To play this game you need to type rock, paper or scissors. You can view the results in the console after the 5th round.
--- index.js
+++ index.js
@@ -4,7 +4,6 @@ let computerScore = 0;
 function getComputerChoice() {
   let comChoice = 0; //computer choice
   comChoice = Math.floor(Math.random() * (3 - 1 + 1) + 1);
-  console.log(comChoice);
   return comChoice;
 }
 
@@ -17,6 +16,8 @@ function getHumanChoice() {
 
 function playRound(humanChoice, computerChoice) {
   let human = humanChoice.toLowerCase();
+
+  score = 0;
 
   switch (human) {
     case "rock":
@@ -31,39 +32,59 @@ function playRound(humanChoice, computerChoice) {
     default:
       break;
   }
-  console.log("word "+humanChoice);
-  console.log("numb " + human);
+
 
   if (human === 1 && computerChoice === 2) {
     console.log("Human wins, Rock beats scissors");
+    score = 1;
   } else if (human === 2 && computerChoice === 3) {
     console.log("Human wins, Scissors beats paper");
+    score = 1;
   } else if (human === 3 && computerChoice === 1) {
     console.log("Human wins, Paper beats rock");
+    score = 1;
   } else if (human === 1 && computerChoice === 3) {
-    console.log("You lose, Rock beats scissors");
+    console.log("You lose, Paper beats rock");
   } else if (human === 2 && computerChoice === 1) {
-    console.log("You lose, Scissors beats paper");
+    console.log("You lose, Rock beats scissors");
   } else if (human === 3 && computerChoice === 2) {
-    console.log("you lose, Paper beats rock");
+    console.log("you lose, Scissors beats paper");
   } else if (human === computerChoice) {
     console.log("Tie");
-  } else {
-    console.log("You lose, ");
+    score = 2;
   }
+
   return score;
 }
 
+function playGame() {
+  let score = 0;
 
+  for (let i = 0; i < 5; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    score = playRound(humanSelection, computerSelection);
 
-function playGame(){
+    if (score === 1) {
+      humanScore++;
+    } else if (score === 2) {
+    } else {
+      computerScore++;
+    }
 
-let score = 0;
+    console.log(
+      "Human score: " + humanScore + "\nComputer Score: " + computerScore);
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-score = playRound(humanSelection,computerSelection);  
+    score = 0;
+  }
 
-
-
+  if (humanScore > computerScore) {
+    console.log("Yay you won!");
+  }else if(humanScore === computerScore){
+    console.log("you tied? how");
+  }else{
+    console.log("you lost.");
+  }
 }
+
+playGame();
