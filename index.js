922f9e6641302c353ca1be32a9325ebf3c26db6c
joshaw807 <joshaw807@gmail.com>
2025-04-29 21:10:44 -0400

    added players and computer choice.

--- index.js
+++ index.js
@@ -1,24 +1,69 @@
+let humanScore = 0;
+let computerScore = 0;
 
+function getComputerChoice() {
+  let comChoice = 0; //computer choice
+  comChoice = Math.floor(Math.random() * (3 - 1 + 1) + 1);
+  console.log(comChoice);
+  return comChoice;
+}
 
+function getHumanChoice() {
+  let humChoice = 0; //human choice
+  humChoice = prompt("Enter your choice, Rock, Paper, Scissors");
 
+  return humChoice;
+}
 
+function playRound(humanChoice, computerChoice) {
+  let human = humanChoice.toLowerCase();
 
+  switch (human) {
+    case "rock":
+      human = 1;
+      break;
+    case "scissors":
+      human = 2;
+      break;
+    case "paper":
+      human = 3;
+      break;
+    default:
+      break;
+  }
+  console.log("word "+humanChoice);
+  console.log("numb " + human);
 
-function getComputerChoice(){
-    let comChoice = 0; //computer choice
-    choice = Math.floor(Math.random() * (3 - 1 + 1) + 1);
-    console.log(choice);
-    return comChoice
+  if (human === 1 && computerChoice === 2) {
+    console.log("Human wins, Rock beats scissors");
+  } else if (human === 2 && computerChoice === 3) {
+    console.log("Human wins, Scissors beats paper");
+  } else if (human === 3 && computerChoice === 1) {
+    console.log("Human wins, Paper beats rock");
+  } else if (human === 1 && computerChoice === 3) {
+    console.log("You lose, Rock beats scissors");
+  } else if (human === 2 && computerChoice === 1) {
+    console.log("You lose, Scissors beats paper");
+  } else if (human === 3 && computerChoice === 2) {
+    console.log("you lose, Paper beats rock");
+  } else if (human === computerChoice) {
+    console.log("Tie");
+  } else {
+    console.log("You lose, ");
+  }
+  return score;
 }
 
 
-function getHumanChoice(){
-    let humChoice = 0; //human choice
-    humChoice = prompt("Enter your choice, Rock, Paper, Scissors");
-    console.log(humChoice);
 
-    return humChoice;
+function playGame(){
+
+let score = 0;
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+score = playRound(humanSelection,computerSelection);  
+
+
+
 }
-
-
-getHumanChoice();
